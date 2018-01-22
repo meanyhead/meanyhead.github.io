e5f9a1eb447757a90fb0cf785da38a6faf98eb88
meanyhead <ichbineinski@gmail.com>
2018-01-22 15:08:10 -0800

    no hues

--- body_background.js
+++ body_background.js
@@ -1,5 +1,5 @@
 // the page this is loaded on must load randomColor.js beforehand
-var random_color1 = randomColor({luminosity: 'dark', hue: 'yellow'});
-var random_color2 = randomColor({luminosity: 'light', hue: 'pink'});
+var random_color1 = randomColor({luminosity: 'dark'});
+var random_color2 = randomColor({luminosity: 'light'});
 var gradient_string = 'linear-gradient(to bottom right, ' + random_color1 + ', ' + random_color2 + ')';
 document.body.style.background = gradient_string;
