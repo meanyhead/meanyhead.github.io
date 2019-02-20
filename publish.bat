84fef5e575b57a844d2c78910a006d0727f6856f
meanyhead <ichbineinski@gmail.com>
2019-02-20 09:10:55 -0800

    auto Wed 09:10

--- publish.bat
+++ publish.bat
@@ -1,3 +1,4 @@
-for /f %%i in ('c:\cygwin64\bin\date.exe +"%%Y%%m%%d%%H%%M%%S"') do set datetime=%%i
+for /f %%i in ('date /t') do set date=%%i
+for /f %%i in ('time /t') do set time=%%i
 git add .
-git commit -m "auto %datetime%"
+git commit -m "auto %date% %time%"
