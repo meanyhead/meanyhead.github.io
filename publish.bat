f3310c4cd3a697c1d2832fb66047579908696013
meanyhead <ichbineinski@gmail.com>
2019-02-20 09:28:07 -0800

    abort

--- publish.bat
+++ publish.bat
@@ -1,7 +1,7 @@
+git add .
 date /t > publish_date.txt
 time /t > publish_time.txt
 set /p DATEVAR=<publish_date.txt
 set /p TIMEVAR=<publish_time.txt
-git add .
 git commit -m "auto %DATEVAR% %TIMEVAR%"
 git push
